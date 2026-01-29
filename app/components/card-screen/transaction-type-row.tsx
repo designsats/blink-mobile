fbcadc030a2223200e62a7286b94ce4041354cf7
Esaú Gómez) <alfonsoesaugomez@gmail.com>
2026-01-28 18:51:59 -0600

    feat(card): add TransactionTypeRow component

---
+++ app/components/card-screen/transaction-type-row.tsx
@@ -0,0 +1,52 @@
+import React from "react"
+import { ListItem, makeStyles } from "@rn-vui/themed"
+
+import { Switch } from "@app/components/atomic/switch"
+
+type TransactionTypeRowProps = {
+  title: string
+  description: string
+  value: boolean
+  onValueChange: () => void
+}
+
+export const TransactionTypeRow: React.FC<TransactionTypeRowProps> = ({
+  title,
+  description,
+  value,
+  onValueChange,
+}) => {
+  const styles = useStyles()
+  return (
+    <ListItem containerStyle={styles.listItemContainer}>
+      <ListItem.Content>
+        <ListItem.Title style={styles.listItemTitle}>{title}</ListItem.Title>
+        <ListItem.Subtitle style={styles.listItemSubtitle}>
+          {description}
+        </ListItem.Subtitle>
+      </ListItem.Content>
+      <Switch value={value} onValueChange={onValueChange} />
+    </ListItem>
+  )
+}
+
+const useStyles = makeStyles(({ colors }) => ({
+  listItemContainer: {
+    backgroundColor: colors.transparent,
+    paddingVertical: 8,
+    paddingLeft: 14,
+    paddingRight: 10,
+    minHeight: 0,
+  },
+  listItemTitle: {
+    color: colors.black,
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: "400",
+  },
+  listItemSubtitle: {
+    color: colors.grey2,
+    fontSize: 14,
+    lineHeight: 16,
+  },
+}))
