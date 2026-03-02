598b79ffc36f04be2cb52b537322abeb30bc743e
designsats <andrejcibik@gmail.com>
2026-03-02 14:31:26 -0300

    Body padding variable readded

--- app/config/appinfo.ts
+++ app/config/appinfo.ts
@@ -25,3 +25,4 @@ export const TELEGRAM_CALLBACK_PATH = "auth/passport-callback"
 export const HIDDEN_AMOUNT_PLACEHOLDER = "****"
 export const APPROXIMATE_PREFIX = "~"
 export const MASK_CHAR = "•"
+export const BODY_PADDING = 20
